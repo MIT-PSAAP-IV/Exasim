d46ebfb71a97cfc8cb157c14097c6595d94f1844
Ngoc-Cuong Nguyen <70904303+exapde@users.noreply.github.com>
2026-06-02 23:27:36 -0400

    Update builtin app

--- apps/builtinlibrary/main.cpp
+++ apps/builtinlibrary/main.cpp
@@ -1,3 +1,14 @@
+/*
+Build this app from apps/builtinlibrary after configuring Exasim:
+
+  cmake -B build -DExasim_DIR=/path/to/Exasim
+  cmake --build build
+
+Run with an input file that uses a built-in model ID:
+
+  mpirun -np 4 build/exasimapp /path/to/pdeapp.txt
+*/
+
 #include "ExasimSolverSetup.hpp"
 #include "../../backend/Model/BuiltIn/builtinlibprovider.cpp"
 
@@ -12,4 +23,3 @@ int main(int argc, char** argv)
     ExasimSolver solver;
     return RunExasimSolver(solver, argc, argv, comm);
 }
-
